0e56e2512cc8dd1e8a77e0cf3a1971526300846a
Plesca Razvan <razvanplesca12@gmail.com>
2025-08-05 18:14:06 +0300

    Implemented second part

--- src/main/java/org/example/Contact.java
+++ src/main/java/org/example/Contact.java
@@ -2,4 +2,10 @@ package org.example;
 
 public class Contact {
     private String number;
+    private String name;
+
+    public Contact(String number, String name) {
+        this.number = number;
+        this.name = name;
+    }
 }
